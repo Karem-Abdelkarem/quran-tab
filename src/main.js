96270bb5c88e3d071d79d7dd119447a3628af04e
Kareem Abdelkarem <km6263458@gmail.com>
2025-08-14 16:07:12 +0300

    feat: add sections animation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import "./style.scss";
-import { labelClickEvent } from "./utils";
+import { labelClickEvent, sectionsAnimation } from "./utils";
 
 const labelElements = document.querySelectorAll("label");
+const sectionsElements = document.querySelectorAll("section");
 
 labelElements.forEach(labelClickEvent);
+sectionsAnimation(sectionsElements);
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,16 @@ export const labelClickEvent = (el) => {
     if (e.key === "Enter") el.click();
   });
 };
+
+export const sectionsAnimation = (sectionsElements) => {
+  const options = { threshold: 0.4 };
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      }
+    });
+  };
+  const observer = new IntersectionObserver(callback, options);
+  sectionsElements.forEach((element) => observer.observe(element));
+};
